fe853120ab370cb4b3b37244ce0a4d3fe0bed433
RiyaSingh-2310 <riyasanam.singh@gmail.com>
2026-06-11 14:21:54 +0530

    enhance helmet configuration to include cross-origin resource policy

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ import surveyRoutes from './routes/surveyRoutes.js';
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+}));
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
