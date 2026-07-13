0fbf1d73afc8500d5b6d382895fafd0ace5bb85e
akshat123644532 <akshatsolanki106@gmail.com>
2026-07-13 11:31:42 +0530

    cahange in panelists

--- controllers/Panelistcontroller.js
+++ controllers/Panelistcontroller.js
@@ -266,9 +266,12 @@ export const toggleStatus = async (req, res) => {
         const panelist = await Panelist.findById(id);
         if (!panelist) return res.status(404).json({ success: false, message: "Panelist not found!" });
 
+        
         await Panelist.toggleStatus(id, status);
         return res.status(200).json({ success: true, message: `Status updated to ${status}!` });
     } catch (error) {
         return res.status(500).json({ success: false, message: "Server error!", error: error.message });
     }
 };
+
+
