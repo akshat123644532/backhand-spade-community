784952b8aad7bb14bb356832ff101d6f67a7f50c
akshat123644532 <akshatsolanki106@gmail.com>
2026-07-13 10:50:35 +0530

    cahnges email pass

--- config/mailer.js
+++ config/mailer.js
@@ -3,10 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtppro.zoho.com',
+    port: 465,
+    secure: true, // SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("MAILER CONNECTION FAILED:", error);
+    } else {
+        console.log("MAILER READY TO SEND EMAILS ✅");
     }
 });
 
