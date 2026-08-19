eb259338bbdbcf876f79f1f0563032e7ca80fcaa
akshat123644532 <akshatsolanki106@gmail.com>
2026-08-19 18:57:36 +0530

    fix

--- routes/supplierRedirectRoutes.js
+++ routes/supplierRedirectRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import { 
-    getProjectReport, 
-    downloadProjectReportCsv 
-} from '../controllers/reportController.js'; 
+import { getProjectReport, downloadProjectReportCsv } from '../controllers/reportController.js';
+import verifyToken from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/:id/report', getProjectReport);
-router.get('/:id/report/export/csv', downloadProjectReportCsv);
+router.get('/:id/report', verifyToken, getProjectReport);
+router.get('/:id/report/export/csv', verifyToken, downloadProjectReportCsv);
 
 export default router;
